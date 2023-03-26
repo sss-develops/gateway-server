0f7ccb73f4f712d14bd7b2c6b2397b8855815b34
Bangseungjae <tmdwo5825@naver.com>
2023-03-26 23:29:50 +0900

    first commit

---
+++ src/test/java/goorm/project/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package goorm.project.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
