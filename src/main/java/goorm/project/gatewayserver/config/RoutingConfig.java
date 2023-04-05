80bba6c466100913e2ca4e7d8ba06abe890fcf26
Bangseungjae <tmdwo5825@naver.com>
2023-04-05 22:26:38 +0900

    feature: 필터를 주가한다. - router에 추가할 게이트 웨이 로직을 정리한다.

--- src/main/java/goorm/project/gatewayserver/config/RoutingConfig.java
+++ src/main/java/goorm/project/gatewayserver/config/RoutingConfig.java
@@ -1,38 +1,51 @@
 package goorm.project.gatewayserver.config;
 
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 import java.util.Date;
 
-        @Configuration
-        public class RoutingConfig {
+@Configuration
+public class RoutingConfig {
 
-            @Bean
-            public RouteLocator routeLocator(RouteLocatorBuilder builder) {
-                return builder.routes()
-                        .route(p -> p
-                                .path("/query-server/**")
-                                .filters(f -> f.rewritePath("/query-server/(?<segment>.*)", "/${segment}")
-                                        .addResponseHeader("X-Response-Time", new Date().toString()))
-                                .uri("lb://QUERY-SERVER")
-                        )
-                        .route(p -> p
-                                .path("/authentication-server/**")
-                                .filters(f -> f.rewritePath("/authentication-server/(?<segment>.*)", "/${segment}")
-                                        .addResponseHeader("X-Response-Time", new Date().toString()))
-                                .uri("lb://AUTHENTICATION-SERVER")
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+
+                        .path("/query-server/**")
+                        .filters(f -> f.rewritePath("/query-server/(?<segment>.*)", "/${segment}")
+                                .filters((exchange, chain) ->  gatewayFilter(exchange, chain))
+                                .addResponseHeader("X-Response-Time", new Date().toString()))
+                        .uri("lb://QUERY-SERVER")
                 )
-                        .route(p -> p
-                                .path("/news-write-server/**")
-                                .filters(f -> f.rewritePath("/news-write-server/(?<segment>.*)", "/${segment}")
-                                        .addResponseHeader("X-Response-Time", new Date().toString()))
-                                .uri("lb://NEWS-WRITE-SERVER")
+                .route(p -> p
+                        .path("/authentication-server/**")
+                        .filters(f -> f.rewritePath("/authentication-server/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", new Date().toString()))
+                        .uri("lb://AUTHENTICATION-SERVER")
+                )
+                .route(p -> p
+                        .path("/news-write-server/**")
+                        .filters(f -> f.rewritePath("/news-write-server/(?<segment>.*)", "/${segment}")
+                                .filters((exchange, chain) ->  gatewayFilter(exchange, chain))
+                                .addResponseHeader("X-Response-Time", new Date().toString()))
+                        .uri("lb://NEWS-WRITE-SERVER")
 
-                        )
-
-                        .build();
+                )
+                .build();
     }
+
+    private Mono<Void> gatewayFilter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        /**
+         * todo: please 준우형~~
+         */
+        return null;
+    }
+
 }
