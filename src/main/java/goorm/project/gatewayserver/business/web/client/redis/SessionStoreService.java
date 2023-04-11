72cfc541ff48768a63cf132ff6f4ad10285ea27d
Bangseungjae <tmdwo5825@naver.com>
2023-04-11 18:37:48 +0900

    bug: Redis 설정을 Reactive로 바꿔준다.

--- src/main/java/goorm/project/gatewayserver/business/web/client/redis/SessionStoreService.java
+++ src/main/java/goorm/project/gatewayserver/business/web/client/redis/SessionStoreService.java
@@ -2,26 +2,24 @@ package goorm.project.gatewayserver.business.web.client.redis;
 
 import goorm.project.gatewayserver.common.domain.error.CommonTypeException;
 import goorm.project.gatewayserver.common.exception.common.LoginForbiddenException;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ReactiveRedisOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import javax.annotation.Resource;
+import reactor.core.publisher.Mono;
 
 @Service
 public class SessionStoreService implements RedisSessionService{
 
-    @Resource(name = "sessionRedisTemplate")
-    private final StringRedisTemplate stringRedisTemplate;
+    private final ReactiveRedisOperations<String, String> redisOperations;
 
-    public SessionStoreService(StringRedisTemplate stringRedisTemplate) {
-        this.stringRedisTemplate = stringRedisTemplate;
+    public SessionStoreService(ReactiveRedisOperations redisOperations) {
+        this.redisOperations = redisOperations;
     }
 
     @Override
     @Transactional
     public void isLoginUser(Long memberId) {
-        String session = stringRedisTemplate.opsForValue().get(getSessionsKey(memberId));
+        Mono<String> session = redisOperations.opsForValue().get(getSessionsKey(memberId));
 
         if (session == null) {
             throw LoginForbiddenException.of(CommonTypeException.NOT_FOUND_SESSION_MEMBER);
