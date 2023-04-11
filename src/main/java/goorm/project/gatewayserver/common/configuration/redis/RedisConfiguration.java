b0372b8d7b992963147a95a2ac2303e99266653f
Bangseungjae <tmdwo5825@naver.com>
2023-04-11 17:45:46 +0900

    refactor: cluster용으로 redis 설정을 수정한다.

--- src/main/java/goorm/project/gatewayserver/common/configuration/redis/RedisConfiguration.java
+++ src/main/java/goorm/project/gatewayserver/common/configuration/redis/RedisConfiguration.java
@@ -3,8 +3,8 @@ package goorm.project.gatewayserver.common.configuration.redis;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -22,9 +22,8 @@ public class RedisConfiguration {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setHostName(host);
-        redisStandaloneConfiguration.setPort(port);
+        RedisClusterConfiguration redisStandaloneConfiguration = new RedisClusterConfiguration();
+        redisStandaloneConfiguration.clusterNode(host, port);
         return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
